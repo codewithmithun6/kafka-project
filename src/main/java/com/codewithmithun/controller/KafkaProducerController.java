edd768f87bb2da0350cbd5560550dc3c1c9d019c
codewithmithun6 <codewithmithun6@gmail.com>
2024-08-01 18:50:48 +0530

    Spring boot with kafka

---
+++ src/main/java/com/codewithmithun/controller/KafkaProducerController.java
@@ -0,0 +1,29 @@
+package com.codewithmithun.controller;
+
+
+import com.codewithmithun.kafka.KafkaProducer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/kafka")
+public class KafkaProducerController {
+
+    @Autowired
+    private KafkaProducer kafkaProducer;
+
+    @GetMapping("/health")
+    public String health(){
+        return "OK";
+    }
+
+    @GetMapping("/publish")
+    public ResponseEntity<String> publish(@RequestParam("message") String message){
+        kafkaProducer.sendMessage(message);
+        return ResponseEntity.ok("Message sent to kafka topic");
+    }
+}
